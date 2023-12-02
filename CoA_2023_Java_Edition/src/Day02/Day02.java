d018101c3a3d3b3f6a287b9195d9cb08ffd9aa74
Robert König <mailto.koenig.robert@gmail.com>
2023-12-02 09:22:49 +0100

    Day02 - Initial Push

---
+++ CoA_2023_Java_Edition/src/Day02/Day02.java
@@ -0,0 +1,38 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class Day02 {
+	private int solution1 = 0;
+	private int solution2 = 0;
+	static Scanner in;
+	
+	public static void main(String[] args)   {
+		Day02 day02 = new Day02();
+		Day02.solution(day02);
+		
+	}
+
+	private static void solution(Day02 day02) {
+		
+	}
+
+	
+	
+	public int getSolution1() {
+		return solution1;
+	}
+
+	public void setSolution1(int solution1) {
+		this.solution1 = solution1;
+	}
+
+	public int getSolution2() {
+		return solution2;
+	}
+
+	public void setSolution2(int solution2) {
+		this.solution2 = solution2;
+	}
+
+}
