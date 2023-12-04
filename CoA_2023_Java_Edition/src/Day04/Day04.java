039f0b1ed01f282a778f2d780749d50eae2d43f3
Robert König <mailto.koenig.robert@gmail.com>
2023-12-04 09:27:00 +0100

    Day 04 Solution 1

--- CoA_2023_Java_Edition/src/Day04/Day04.java
+++ CoA_2023_Java_Edition/src/Day04/Day04.java
@@ -2,6 +2,7 @@ package Day04;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day04 {
@@ -14,17 +15,15 @@ public class Day04 {
 		Day04 day04 = new Day04();
 		Day04.solution(day04);
 		System.out.println("Solution 1: " + day04.getSolution1());
-		System.out.println("Solution 2: " + day04.getSolution2());
 	}
 
 	private static void solution(Day04 day04) {
 		StringBuilder sb = new StringBuilder();
 		try {
-			in = new Scanner(new FileReader("./src/Day02/Day02.txt"));
+			in = new Scanner(new FileReader("./src/Day04/Day04.txt"));
 			while (in.hasNext()) {
 				sb = new StringBuilder().append(in.nextLine());
 				day04.setSolution1(day04.getSolution1() + checkSingleCard1(sb.toString()));
-				day04.setSolution2(day04.getSolution2() + checkSingleCard2(sb.toString()));
 			}
 		} catch (FileNotFoundException e) {
 			System.err.println("File not Found");
@@ -34,11 +33,23 @@ public class Day04 {
 	}
 
 	private static int checkSingleCard1(String string) {
-			return 0;
-	}
 
-	private static int checkSingleCard2(String string) {
-		return 0;
+		int[] winningNumbers = Arrays.stream(string.split(" \\| ")[0].split(": ")[1]
+				.replace("  ", " ").stripLeading().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int[] randomNumbers = Arrays.stream(string.split(" \\| ")[1]
+				.replace("  ", " ").stripLeading().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		int count = 0;
+		
+		for (int i = 0; i < winningNumbers.length; i++) {
+			for (int j = 0; j < randomNumbers.length; j++) {
+				if (winningNumbers[i] == randomNumbers[j]) {
+					if (count == 0) { count++; } else { count *= 2; }
+					break;
+				}
+			}
+		}	
+		return count;
 	}
 
 	public int getSolution1() {
