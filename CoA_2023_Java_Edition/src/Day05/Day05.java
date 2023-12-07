aceedd6ddf4cac027f403647f8b77f2faa4842ec
Robert König <mailto.koenig.robert@gmail.com>
2023-12-07 08:10:25 +0100

    Day 07 initial push

--- CoA_2023_Java_Edition/src/Day05/Day05.java
+++ CoA_2023_Java_Edition/src/Day05/Day05.java
@@ -2,11 +2,11 @@ package Day05;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.HashMap;
+//import java.util.Arrays;
+//import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+//import java.util.List;
+//import java.util.Map;
 import java.util.Scanner;
 
 public class Day05 {
@@ -41,56 +41,56 @@ public class Day05 {
 	}
 
 	private static int solution1(LinkedList<String> input) {
-		double[] seeds = Arrays.stream(input.get(0).split(": ")[1].split(" ")).mapToDouble(Double::parseDouble).toArray();
+//		double[] seeds = Arrays.stream(input.get(0).split(": ")[1].split(" ")).mapToDouble(Double::parseDouble).toArray();
 		
 		// create Sublists
-		List<String> subListSoil = createSubList(input, "seed-to-soil map:", "soil-to-fertilizer map:");
-		List<String> subListFertilizer = createSubList(input, "soil-to-fertilizer map:", "fertilizer-to-water map:");
-		List<String> subListWater = createSubList(input, "fertilizer-to-water map:", "water-to-light map:");
-		List<String> subListLight = createSubList(input, "water-to-light map:", "light-to-temperature map:");
-		List<String> subListTemperature = createSubList(input, "light-to-temperature map:", "temperature-to-humidity map:");
-		List<String> subListHumidity = createSubList(input, "temperature-to-humidity map:", "humidity-to-location map:");
-		List<String> subListLocation = createSubList(input, "humidity-to-location map:", null);
+//		List<String> subListSoil = createSubList(input, "seed-to-soil map:", "soil-to-fertilizer map:");
+//		List<String> subListFertilizer = createSubList(input, "soil-to-fertilizer map:", "fertilizer-to-water map:");
+//		List<String> subListWater = createSubList(input, "fertilizer-to-water map:", "water-to-light map:");
+//		List<String> subListLight = createSubList(input, "water-to-light map:", "light-to-temperature map:");
+//		List<String> subListTemperature = createSubList(input, "light-to-temperature map:", "temperature-to-humidity map:");
+//		List<String> subListHumidity = createSubList(input, "temperature-to-humidity map:", "humidity-to-location map:");
+//		List<String> subListLocation = createSubList(input, "humidity-to-location map:", null);
 		
-		for (String string : subListFertilizer) {
-			System.out.println(string);
-		}
-		
-		// Test
-		Map<Double, Double> test = new HashMap<>();
-		for (String string : subListFertilizer) {
-			String[] tmp = string.split(" ");
-			double source = Double.parseDouble(tmp[1]);
-			double destination = Double.parseDouble(tmp[0]);
-			for (double i = 0; i < Double.parseDouble(tmp[2]); i++) {
-				test.put(source, destination);
-				source++;
-				destination++;
-			}
-		}
-        
-        //fill map with additional values
-        for (double d = Arrays.stream(seeds).min().getAsDouble(); d <=  Arrays.stream(seeds).max().getAsDouble(); d++) {
-        	if (!test.containsKey(d)) {
-        		test.put(d, d);
-        	}
-        }
-		
-        for (Double i : test.keySet()) {
-            System.out.println(i + " - " + test.get(i));
-		}
+//		for (String string : subListFertilizer) {
+//			System.out.println(string);
+//		}
+//		
+//		// Test
+//		Map<Double, Double> test = new HashMap<>();
+//		for (String string : subListFertilizer) {
+//			String[] tmp = string.split(" ");
+//			double source = Double.parseDouble(tmp[1]);
+//			double destination = Double.parseDouble(tmp[0]);
+//			for (double i = 0; i < Double.parseDouble(tmp[2]); i++) {
+//				test.put(source, destination);
+//				source++;
+//				destination++;
+//			}
+//		}
+//        
+//        //fill map with additional values
+//        for (double d = Arrays.stream(seeds).min().getAsDouble(); d <=  Arrays.stream(seeds).max().getAsDouble(); d++) {
+//        	if (!test.containsKey(d)) {
+//        		test.put(d, d);
+//        	}
+//        }
+//		
+//        for (Double i : test.keySet()) {
+//            System.out.println(i + " - " + test.get(i));
+//		}
 		
 		return 0;
 	}
 
 	
-	private static List<String> createSubList(LinkedList<String> input, String start, String end) {
-		if (end != null) {
-			return input.subList((input.indexOf(start) + 1), (input.indexOf(end)));
-		} else {
-			return input.subList((input.indexOf(start) + 1), input.size());
-		}
-	}
+//	private static List<String> createSubList(LinkedList<String> input, String start, String end) {
+//		if (end != null) {
+//			return input.subList((input.indexOf(start) + 1), (input.indexOf(end)));
+//		} else {
+//			return input.subList((input.indexOf(start) + 1), input.size());
+//		}
+//	}
 
 	public int getSolution1() {
 		return solution1;
---
+++ CoA_2023_Java_Edition/src/Day07/Day07.java
@@ -0,0 +1,71 @@
+package Day07;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day07 {
+	private int solution1 = 0;
+	private int solution2 = 0;
+	static Scanner in;
+
+
+	public static void main(String[] args) {
+		Day07 day07 = new Day07();
+		Day07.solution(day07);
+		System.out.println("Solution 1: " + day07.getSolution1());
+	}
+
+	private static void solution(Day07 day07) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			in = new Scanner(new FileReader("./src/Day04/Day04.txt"));
+			while (in.hasNext()) {
+				sb = new StringBuilder().append(in.nextLine());
+				day07.setSolution1(day07.getSolution1() + checkSingleCard1(sb.toString()));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("File not Found");
+		} finally {
+			in.close();
+		}
+	}
+
+	private static int checkSingleCard1(String string) {
+
+		int[] winningNumbers = Arrays.stream(string.split(" \\| ")[0].split(": ")[1]
+				.replace("  ", " ").stripLeading().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int[] randomNumbers = Arrays.stream(string.split(" \\| ")[1]
+				.replace("  ", " ").stripLeading().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		int count = 0;
+		
+		for (int i = 0; i < winningNumbers.length; i++) {
+			for (int j = 0; j < randomNumbers.length; j++) {
+				if (winningNumbers[i] == randomNumbers[j]) {
+					if (count == 0) { count++; } else { count *= 2; }
+					break;
+				}
+			}
+		}	
+		return count;
+	}
+
+	public int getSolution1() {
+		return solution1;
+	}
+
+	public void setSolution1(int solution1) {
+		this.solution1 = solution1;
+	}
+
+	public int getSolution2() {
+		return solution2;
+	}
+
+	public void setSolution2(int solution2) {
+		this.solution2 = solution2;
+	}
+
+}
